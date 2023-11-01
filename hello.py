0d860acda6ab3993f0db70711bc5e44a70687050
uyh1556 <uyh1556@naver.com>
2023-11-01 14:00:46 +0900

    test

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def hello_world():
-    return "<p>Hello, World!<p>"
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
